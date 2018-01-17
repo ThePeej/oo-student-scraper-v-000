8aebe0c1b9c60c7490b81ba4ce8d2f39629ab297
PJ Frias <peejfrias@gmail.com>
2018-01-17 14:03:57 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     student = {}
-
+    binding.pry
     student[:name] = doc.css("")
     student[:location] = doc.css("")
     student[:prorfile_url] = doc.css("")
