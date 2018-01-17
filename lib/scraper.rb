ea15fbb7c2ceb2dbc08e1a200049ac3a7367f2a6
PJ Frias <peejfrias@gmail.com>
2018-01-17 14:01:42 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,12 +5,15 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    
+    student = {}
+
+    student[:name] = doc.css("")
+
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
